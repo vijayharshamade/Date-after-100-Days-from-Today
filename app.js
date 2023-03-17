071762995e58dcd778aeb9f3a0cf99a058b4b686
Vijay Harsha Made <87754774+vijayharshamade@users.noreply.github.com>
2023-03-18 01:54:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+//https://date-fns.org/v2.29.3/docs/addDays
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const dateTime = new Date();
+  const result = addDays(
+    new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+
+module.exports = app;
